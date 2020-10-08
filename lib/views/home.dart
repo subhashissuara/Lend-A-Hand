021835057097e69748ad83148edfee5fade452bb
Subhashis Suara <subhashis.suara999@gmail.com>
2020-10-08 21:09:30 +0530

    Create Home View

---
+++ lib/views/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Reports'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),  
+            onPressed: () {
+              
+            }
+            )
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(right: 25, bottom: 25),
+        child: FloatingActionButton(
+          onPressed: () {
+            
+          },
+          tooltip: 'Report',
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
